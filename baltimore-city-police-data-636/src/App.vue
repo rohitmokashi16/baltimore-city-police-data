f82b5e2a92916d82ab3219466b6983257c6acebe
Jason <jass2@umbc.edu>
2021-10-11 19:22:40 -0400

    npm app init

---
+++ baltimore-city-police-data-636/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <TabView/>
+  </div>
+</template>
+
+<script>
+import TabView from './components/TabView.vue'
+export default {
+  name: 'App',
+  components: {
+    TabView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ baltimore-city-police-data-636/src/components/TabView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <b-tabs content-class="mt-3">
+                 <b-tab no-body title="Picture 1">
+        <b-card-img bottom src="https://picsum.photos/600/200/?image=21" alt="Image 21"></b-card-img>
+        <b-card-footer>Picture 1 footer</b-card-footer>
+      </b-tab>
+
+      <b-tab no-body title="Picture 2">
+        <b-card-img bottom src="https://picsum.photos/600/200/?image=25" alt="Image 25"></b-card-img>
+        <b-card-footer>Picture 2 footer</b-card-footer>
+      </b-tab>
+
+      <b-tab no-body title="Picture 3">
+        <b-card-img bottom src="https://picsum.photos/600/200/?image=26" alt="Image 26"></b-card-img>
+        <b-card-footer>Picture 3 footer</b-card-footer>
+      </b-tab>
+
+        </b-tabs>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
