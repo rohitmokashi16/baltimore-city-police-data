27b5fb814cf2e28c477fb7640cfcccc34645c9c2
Jason <jass2@umbc.edu>
2021-10-12 15:24:44 -0400

    Implemented template for d3 and tableau

--- baltimore-city-police-data-636/src/components/TabView.vue
+++ baltimore-city-police-data-636/src/components/TabView.vue
@@ -1,19 +1,16 @@
 <template>
-    <div>
-        <b-tabs content-class="mt-3">
-                 <b-tab no-body title="Picture 1">
-        <b-card-img bottom src="https://picsum.photos/600/200/?image=21" alt="Image 21"></b-card-img>
-        <b-card-footer>Picture 1 footer</b-card-footer>
-      </b-tab>
+<div>
+    <b-tabs content-class="mt-3">
+         <b-tab no-body title="Picture 1">
+           <D3Impl/>
+                </b-tab>
 
       <b-tab no-body title="Picture 2">
-        <b-card-img bottom src="https://picsum.photos/600/200/?image=25" alt="Image 25"></b-card-img>
-        <b-card-footer>Picture 2 footer</b-card-footer>
+          <Tableau/>
       </b-tab>
 
       <b-tab no-body title="Picture 3">
-        <b-card-img bottom src="https://picsum.photos/600/200/?image=26" alt="Image 26"></b-card-img>
-        <b-card-footer>Picture 3 footer</b-card-footer>
+          <Matplotlib/>
       </b-tab>
 
         </b-tabs>
@@ -21,7 +18,13 @@
 </template>
 
 <script>
+import D3Impl from './d3.vue';
+import Tableau from './tableu.vue';
+import Matplotlib from './matplotlib.vue'
 
+export default {
+  components: {D3Impl, Tableau, Matplotlib},
+}
 </script>
 
 <style scoped>
