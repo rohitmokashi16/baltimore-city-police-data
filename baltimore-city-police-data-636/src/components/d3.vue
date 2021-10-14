24e93cfed050d11831a08fb688c748ce2c707932
rohit.mokashi <rohit.mokashi@msm.digital>
2021-10-14 02:02:44 -0400

    [RM] #main Added D3.js implementation

--- baltimore-city-police-data-636/src/components/d3.vue
+++ baltimore-city-police-data-636/src/components/d3.vue
@@ -1,99 +1,108 @@
 <template>
-   <div>
-       <div class="row justify-center">
-       <p>D3 impl <br></p>
-       </div>
-       <div id="arc"/>
-   </div>
+  <div>
+    <div>
+      <div class="row justify-center">
+        <p>D3 impl <br /></p>
+      </div>
+      <div id="arc" />
+    </div>
+  </div>
 </template>
 
 <script>
 import * as d3 from "d3";
+import * as topojson from "topojson";
+import crimeData from "@/assets/crime-data.json";
+import baltimoreCity from "@/assets/baltimore-city-topo.json";
+// import boston from "@/assets/boston_neighborhoods.json";
 
 export default {
   name: "D3Impl",
   data() {
     return {
-      gdp: [
-        {country: "USA", value: 20.5 },
-        {country: "China", value: 13.4 },
-        {country: "Germany", value: 4.0 },
-        {country: "Japan", value: 4.9 },
-        {country: "France", value: 2.8 }
-      ]
+      baltimoreCity,
+      crimeData,
     };
   },
-mounted() {
+  mounted() {
     this.generateArc();
   },
   methods: {
     generateArc() {
-      const w = 500;
-      const h = 500;
+      var width = 1000;
+      var height = 1100;
 
-      const svg = d3
-        .select("#arc")
+      var projection = d3
+        .geoAlbers()
+        .center([0, 39.3])
+        .rotate([76.6, 0])
+        .parallels([38, 40])
+        .scale(350000)
+        .translate([600, 450]);
+
+      var path = d3.geoPath().projection(projection);
+
+      var svg = d3
+        .select("div")
         .append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .attr("display", "inline-block")
-        .attr("margin", "0 auto");
+        .attr("width", width)
+        .attr("height", height);
 
-      const sortedGDP = this.gdp.sort((a, b) => (a.value > b.value ? 1 : -1));
-      const color = d3.scaleOrdinal(d3.schemeDark2);
+      var base = svg.append("g");
 
-      const max_gdp = d3.max(sortedGDP, o => o.value);
+      var border = svg.append("g");
 
-      const angleScale = d3
-        .scaleLinear()
-        .domain([0, max_gdp])
-        .range([0, 1.5 * Math.PI]);
+      var crime = svg.append("g"); 
 
-      const arc = d3
-        .arc()
-        .innerRadius((d, i) => (i + 1) * 25)
-        .outerRadius((d, i) => (i + 2) * 25)
-        .startAngle(angleScale(0))
-        .endAngle(d => angleScale(d.value));
+      var baltimore = topojson.feature(
+        this.baltimoreCity,
+        this.baltimoreCity.objects.neighborhoods
+      );
 
-      const g = svg.append("g");
+      var mesh = topojson.mesh(
+        this.baltimoreCity,
+        this.baltimoreCity.objects.neighborhoods
+      );
 
-      g.selectAll("path")
-        .data(sortedGDP)
+      base
+        .append("path")
+        .datum(baltimore)
+        .attr("class", "base")
+        .attr("d", path);
+
+      border
+        .append("path")
+        .datum(mesh)
+        .attr("d", path)
+        .attr("class", "boundary");
+
+      crime
+        .selectAll("path")
+        .data(this.crimeData.features)
         .enter()
         .append("path")
-        .attr("d", arc)
-        .attr("fill", (d, i) => color(i))
-        .attr("stroke", "#FFF")
-        .attr("stroke-width", "1px")
-        .on("mouseenter", function() {
-          d3.select(this)
-            .transition()
-            .duration(200)
-            .attr("opacity", 0.5);
-        })
-        .on("mouseout", function() {
-          d3.select(this)
-            .transition()
-            .duration(200)
-            .attr("opacity", 1);
-        });
+        .attr("class", "dot")
+        .attr("d", path);
 
-      g.selectAll("text")
-        .data(this.gdp)
-        .enter()
-        .append("text")
-        .text(d => `${d.country} -  ${d.value} Trillion`)
-        .attr("x", -150)
-        .attr("dy", -8)
-        .attr("y", (d, i) => -(i + 1) * 25);
-
-      g.attr("transform", "translate(200,300)");
-  }
-  }
-}
+      return svg.node();
+    },
+  },
+};
 </script>
 
-<style scoped>
-
+<style >
+.base {
+  fill: #ddc;
+}
+.boundary {
+  fill: #ddc;
+  stroke: #fff;
+  stroke-dasharray: 2, 2;
+  stroke-linejoin: round;
+}
+.dot {
+  fill: rgb(182, 18, 18);
+  fill-opacity: 0.45;
+  /* stroke: #999; */
+}
 </style>
