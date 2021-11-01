4be886476949fc26f795b8a759ecb47070d1df75
Jason <jass2@umbc.edu>
2021-11-01 03:59:28 -0400

    backend added with flask & vue router to connnect

--- baltimore-city-police-data-636/src/App.vue
+++ baltimore-city-police-data-636/src/App.vue
@@ -1,23 +1,12 @@
 <template>
   <div id="app">
-    <div class="h2 row">
-      <p>Baltimore City Police Data CMSC 636</p>
-    </div>
-    <img alt="Vue logo" src="./assets/logo.png">
-     <div class="h6 row">
-      <p>Powered By Vue</p>
-    </div>
-    <TabView/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import TabView from './components/TabView.vue'
 export default {
   name: 'App',
-  components: {
-    TabView
-  }
 }
 </script>
 
---
+++ baltimore-city-police-data-636/src/components/Terminal.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <div class="h2 row">
+      <p>Baltimore City Police Data CMSC 636</p>
+    </div>
+    <img alt="Vue logo" src="./assets/logo.png">
+     <div class="h6 row">
+      <p>Powered By Vue</p>
+    </div>
+    <TabView/>
+    </div>
+</template>
+<script>
+import TabView from './components/TabView.vue'
+export default {
+  name: 'App',
+  components: {
+    TabView
+  }
+}
+</script>
+
