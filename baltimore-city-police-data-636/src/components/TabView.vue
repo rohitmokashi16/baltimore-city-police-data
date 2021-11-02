a7c61613c27706c17e66d342ebacbda0e9817ede
Jason <jass2@umbc.edu>
2021-11-01 23:05:24 -0400

    everything hooked up for images and frontend to backend connection

--- baltimore-city-police-data-636/src/components/TabView.vue
+++ baltimore-city-police-data-636/src/components/TabView.vue
@@ -1,15 +1,11 @@
 <template>
 <div>
     <b-tabs content-class="mt-3">
-         <b-tab no-body title="Picture 1">
+         <b-tab no-body title="D3 Progress">
            <D3Impl/>
                 </b-tab>
 
-      <b-tab no-body title="Picture 2">
-          <Tableau/>
-      </b-tab>
-
-      <b-tab no-body title="Picture 3">
+      <b-tab no-body title="Python Progress">
           <Matplotlib/>
       </b-tab>
 
@@ -19,11 +15,10 @@
 
 <script>
 import D3Impl from './d3.vue';
-import Tableau from './tableu.vue';
 import Matplotlib from './matplotlib.vue'
 
 export default {
-  components: {D3Impl, Tableau, Matplotlib},
+  components: {D3Impl, Matplotlib},
 }
 </script>
 
--- baltimore-city-police-data-636/src/components/matplotlib.vue
+++ baltimore-city-police-data-636/src/components/matplotlib.vue
@@ -1,11 +1,37 @@
 <template>
     <div>
-      
+      <p> Python Visualizations </p>
+       <b-img v-if="imageFromPython" :src="'data:image/png;base64,'+ imageFromPython"/>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 
+export default {
+    name: "MatPlotLib",
+    data () {
+        return {
+            imageFromPython: null
+        }
+    },
+    async created () {
+        await this.getImage()
+    },
+    methods: {
+        getImage() {
+            const path = 'http://localhost:5000/vue';
+            axios.get(path)
+                .then((res) => {
+                this.imageFromPython = res.data;
+            })
+            .catch((error) => {
+             // eslint-disable-next-line
+            console.error(error);
+        });
+    },
+    }
+}
 </script>
 
 <style scoped>
