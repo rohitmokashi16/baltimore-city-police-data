21a1498faf84ef9cb572d2d4ce6bcdac3c55f567
Jason <jass2@umbc.edu>
2021-11-09 01:22:56 -0500

    updated router

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  base: 'vis636-baltcity-police.herokuapp.com',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/ping',
