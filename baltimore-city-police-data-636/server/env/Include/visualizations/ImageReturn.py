1ed8e6970730339d58dadda64036b34b9fd7f201
Jason <jass2@umbc.edu>
2021-11-22 04:32:43 -0500

    force merge

--- baltimore-city-police-data-636/server/env/Include/visualizations/ImageReturn.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-def returnImage ():
-    with open("./env/Include/sample/logo.png", "rb") as image:
-        return base64.b64encode(image.read())
-
-def returnDate ():
-    with open("./env/Include/sample/Part1_Crime_data.csv", "rb") as data:
-        return data
